e49955ca6b513241d15c982f387542496ce591f2
Sam Turner <sam.turner@kokadjo.net>
2025-05-03 23:33:10 -0400

    Implemented header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from '../assets/investment-calculator-logo.png';
+
+
+export default function header () 
+{
+ return (<header id="header">
+ <img src={logo} alt="Money bag logo"/>
+<h1>React Investment Calculator</h1>
+<p>Hello World</p>
+ </header>  ); 
+
+}
